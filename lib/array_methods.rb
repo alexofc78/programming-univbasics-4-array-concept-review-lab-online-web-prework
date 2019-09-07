eaa574844d1f59cbe4585f5bbb14d53d70ddbfb8
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-07 22:17:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,11 +13,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  x = 0
+  x = array[0]
   array.length.times do |index|
-  if array[index] < x
-    x = array[index]
+    if array[index] < x
+      x = array[index]
+    end
   end
-end
-  x 
+  x
 end
